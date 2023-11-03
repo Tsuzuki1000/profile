3c372d7eae3160e75b5eaae43906b2f032b9fb4e
Tsuzuki1000 <halcyon1000000@gmail.com>
2023-11-03 22:54:27 +0900

    改修

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+{
+  document.addEventListener("DOMContentLoaded", function () {
+    var elements = document.querySelectorAll(".fade-in");
+    elements.forEach(function (element, index) {
+        element.style.opacity = "0";
+        element.style.transition = "opacity 5s";
+        setTimeout(function () {
+            element.style.opacity = "1";
+        }, 300 * index); // 適切な遅延を設定
+    });
+});
+}
